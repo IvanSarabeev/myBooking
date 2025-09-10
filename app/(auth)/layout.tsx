b0068a301531ff2c9e6a87e634c661fc57898b04
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-09-10 18:31:49 +0300

    feat: added auth layout and common pages for sign in and up

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-3">
+            <Image
+              src="/icons/logo.svg"
+              alt="logo"
+              width={37}
+              height={37}
+              loading="lazy"
+            />
+
+            <h1 className="text-2xl font-semibold text-white">myBooking</h1>
+          </div>
+
+          <div>{children}</div>
+        </div>
+      </section>
+
+      <section className="auth-illustration">
+        <Image
+          src="/images/auth-illustration.png"
+          alt="auth illustration"
+          height={1000}
+          width={1000}
+          loading="lazy"
+          className="size-full object-cover"
+        />
+      </section>
+    </main>
+  );
+};
+export default Layout;
