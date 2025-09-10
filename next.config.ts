2b4ed5e36a1e9174febb98434b34181e993ea81b
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-09-10 17:13:36 +0300

    chore: added next image remote configs, future book interface and constant content data

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
+/** @type{import('next').NextConfig}*/
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // typedRoutes: true,
 };
 
 export default nextConfig;
