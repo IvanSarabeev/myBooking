b96593ee6a4ec7b59fbd2821dc51e15accd78364
IvanSarabeev <ivan.sarabeev99@gmail.com>
2025-10-12 00:53:18 +0300

    removed unused method

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,5 +1,5 @@
 import { FC, Fragment } from "react";
-import { auth, signOut } from "@/auth";
+import { auth } from "@/auth";
 import { getBorrowedBooks } from "@/lib/actions/user";
 import { redirect } from "next/navigation";
 import BorrowedBooks from "@/components/BorrowedBooks";
@@ -9,18 +9,6 @@ const ProfilePage: FC = async () => {
   const session = await auth();
 
   if (!session?.user?.id) redirect("/sign-in");
-
-  /**
-   * This is an asynchronous function that handles user logout operations.
-   * It uses server-side functionality to securely sign the user out of their session.
-   *
-   * @returns {Promise<void>} A promise that resolves once the user has been successfully logged out.
-   */
-  const handleLogout = async (): Promise<void> => {
-    "use server";
-
-    return await signOut();
-  };
 
   const userId = session.user.id;
   const borrowedBooks = await getBorrowedBooks(userId);
